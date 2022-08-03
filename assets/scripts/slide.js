e5d5b2a7670c96840bfb76354cc95d9aa229c0d2
Jiwoo Jang <104891203+jellyjw@users.noreply.github.com>
2022-08-03 23:53:00 +0900

    script_slide, test

--- assets/scripts/slide.js
+++ assets/scripts/slide.js
@@ -1,20 +1,73 @@
-let currentSlide = 1;
+let currentSlide = 0;
+let currentTmpSlide = 0;
+/* 이런 방법도 있다.
+let btnBoolean = [false, false, false, false,
+                  false, false, false, false,
+                  false, false, false, false];
+*/
+let total = 0;
+let currentSlidePage = false;
 
-const nextSlide = () => {
-  const header = document.getElementById("header");
-  header.style.display = "block";
+document.querySelectorAll(".start_btn").forEach((element) => {
+  element.addEventListener("click", () => {
+    if (currentSlide === 0) {
+      header.style.display = "block";
+    } else if (currentSlide <= 3) {
+      currentTmpSlide = (currentSlide - 1) * 4;
+      for (let i = 1; i <= 4; i++) {
+        document
+          .querySelectorAll(`input[name="btn${currentTmpSlide + i}"`)
+          .forEach((ele, index) => {
+            if (ele.checked === true) {
+              /*
+            switch (index) {
+              case 0:
+                total -= 2
+                break;
+            }
+            */
+              total += ele.value * 1;
+              currentSlidePage = true;
+            }
+          });
+      }
 
-  const currentSlideRef = document.getElementById(`slide_${currentSlide}`);
-  currentSlideRef.classList.remove("activeSlide");
+      if (currentSlidePage === false) {
+        alert("모든 질문에 답을 선택해 주세요.");
+        return false;
+      } else {
+        currentSlidePage = false;
+      }
+    }
+    currentSlide = currentSlide + 1;
 
-  const nextSlideRef = document.getElementById(`slide_${currentSlide + 1}`);
-  nextSlideRef.classList.add("activeSlide");
+    if (currentSlide === 3) {
+      document.querySelector("#test_final_btn").style.display = "block";
+      document.querySelector("#test_next_btn").style.display = "none";
+    }
 
-  currentSlide = currentSlide + 1;
+    if (currentSlide === 4) {
+      sessionStorage.setItem("total", total);
+      location.href = "./result.html";
+    } else {
+      const currentSlideRef = document.getElementById(`slide_${currentSlide}`);
+      currentSlideRef.classList.remove("activeSlide");
 
-  const myBarText = document.getElementById("myBarText");
-  myBarText.innerText = `${currentSlide - 1} 페이지 / 3 페이지`;
+      const nextSlideRef = document.getElementById(`slide_${currentSlide + 1}`);
+      nextSlideRef.classList.add("activeSlide");
 
-  const myBar = document.getElementById("myBar");
-  myBar.style.width = `${((currentSlide - 1) / 3) * 100}%`;
-};
+      const myBarText = document.getElementById("myBarText");
+      myBarText.innerText = `${currentSlide} 페이지 / 3 페이지`;
+
+      const myBar = document.getElementById("myBar");
+      myBar.style.width = `${(currentSlide / 3) * 100}%`;
+    }
+  });
+});
+
+// const buttons = document.querySelectorAll(".btn");
+// for (const btn of buttons) {
+//   btn.addEventListener("click", () => {
+//     btn.style.backgroundColor = "red";
+//   });
+// }
--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -1,5 +1,21 @@
-const score = 0;
+const score = sessionStorage.getItem("total") | 0;
 
-const addScore = (selected) => {
-    score += selected
+if (score > -24 && score <= -12) {
+  document.querySelector(".container > div:nth-child(1)").style.display =
+    "block";
+}
+
+if (score > -12 && score <= 0) {
+  document.querySelector(".container > div:nth-child(2)").style.display =
+    "block";
+}
+
+if (score > 0 && score <= 12) {
+  document.querySelector(".container > div:nth-child(3)").style.display =
+    "block";
+}
+
+if (score > 12 && score <= 24) {
+  document.querySelector(".container > div:nth-child(4)").style.display =
+    "block";
 }
