e0086d75054c7ee4a6669cba774d33c5c23c2675
장지우 <jang-jiwoo@jangjiuui-MacBookPro.local>
2022-08-13 16:47:18 +0900

    final

--- assets/scripts/result.js
+++ assets/scripts/result.js
@@ -7,3 +7,41 @@ const level = params.get("level");
 const result = document.getElementById(`result_level_${level}`);
 
 result.style.display = "block";
+
+// 카카오 공유하기 버튼
+Kakao.init("c0eacf2e129c149837b4eba95b27e79f");
+Kakao.Share.createDefaultButton({
+  container: "#create-kakaotalk-sharing-btn",
+  objectType: "feed",
+  content: {
+    title: "딸기 치즈 케익",
+    description: "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
+    imageUrl:
+      "http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+    link: {
+      mobileWebUrl: "https://developers.kakao.com",
+      webUrl: "https://developers.kakao.com",
+    },
+  },
+  social: {
+    likeCount: 286,
+    commentCount: 45,
+    sharedCount: 845,
+  },
+  buttons: [
+    {
+      title: "웹으로 보기",
+      link: {
+        mobileWebUrl: "https://developers.kakao.com",
+        webUrl: "https://developers.kakao.com",
+      },
+    },
+    {
+      title: "앱으로 보기",
+      link: {
+        mobileWebUrl: "https://developers.kakao.com",
+        webUrl: "https://developers.kakao.com",
+      },
+    },
+  ],
+});
--- assets/scripts/slide.js
+++ assets/scripts/slide.js
@@ -2,78 +2,80 @@ let currentSlide = 1;
 let total = 0;
 let currentSlidePage = true;
 
-const goNextSlide = () => {
-  const currentSlideRef = document.getElementById(`slide_${currentSlide}`);
-  currentSlideRef.classList.remove("activeSlide");
+const slideSetting = (n) => {
+  console.log(n);
+  document.querySelectorAll(".slide").forEach((ele) => {
+    ele.classList.remove("activeSlide");
+  });
 
-  const nextSlideRef = document.getElementById(`slide_${currentSlide + 1}`);
+  console.log(`#slide_${n}`);
+  const nextSlideRef = document.querySelector(`#slide_${n}`);
   nextSlideRef.classList.add("activeSlide");
 
-  const myBarText = document.getElementById("myBarText");
-  myBarText.innerText = `${currentSlide} 페이지 / 3 페이지`;
+  const myBarText = document.querySelector("#myBarText");
+  myBarText.innerText = `${n - 1} 페이지 / 3 페이지`;
 
-  const myBar = document.getElementById("myBar");
-  myBar.style.width = `${(currentSlide / 3) * 100}%`;
-
-  currentSlide += 1;
+  const myBar = document.querySelector("#myBar");
+  myBar.style.width = `${((n - 1) / 3) * 100}%`;
 };
 
-document.querySelectorAll(".start_btn").forEach((element) => {
-  element.addEventListener("click", () => {
-    if (currentSlide === 1) {
-      const headerRef = document.getElementById("header");
-      headerRef.style.display = "block";
-      goNextSlide();
-    } else if (currentSlide <= 4) {
-      for (let i = 1; i <= 4; i++) {
-        let isChecked = false;
-        const questionRef = document.querySelectorAll(
-          `input[name=btn${(currentSlide - 2) * 4 + i}]`
-        );
-        questionRef.forEach((elem) => {
-          if (elem.checked === true) {
-            total += elem.value * 1;
-            isChecked = true;
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".start_btn").forEach((element) => {
+    element.addEventListener("click", () => {
+      if (currentSlide === 1) {
+        const headerRef = document.querySelector("#header");
+        headerRef.style.display = "block";
+        slideSetting(++currentSlide);
+      } else if (currentSlide <= 5) {
+        if (currentSlide === 2) {
+          const prevSlideBtn = document.getElementById("prevSlide");
+          prevSlideBtn.style.display = "inline-block";
+        }
+        for (let i = 1; i <= 4; i++) {
+          let isChecked = false;
+          const questionRef = document.querySelectorAll(
+            `input[name=btn${(currentSlide - 2) * 4 + i}]`
+          );
+          questionRef.forEach((elem) => {
+            if (elem.checked === true) {
+              total += elem.value * 1;
+              isChecked = true;
+            }
+          });
+          if (isChecked === false) {
+            alert("모든 질문에 응답을 완료해주세요.");
+            return;
           }
-        });
-        if (isChecked === false) {
-          alert("모든 질문에 응답을 완료해주세요.");
-          return;
         }
-      }
-      if (currentSlide !== 4) {
-        if (currentSlide === 3) {
-          document.getElementById("test_next_btn").style.display = "none";
-          document.getElementById("test_final_btn").style.display = "block";
-        }
-        goNextSlide();
-      } else {
-        if (total < -12) {
-          location.href = "./result.html?level=1";
-        } else if (total < 0) {
-          location.href = "./result.html?level=2";
-        } else if (total < 12) {
-          location.href = "./result.html?level=3";
+        if (currentSlide !== 4) {
+          if (currentSlide === 3) {
+            document.getElementById("test_next_btn").style.display = "none";
+            document.getElementById("test_final_btn").style.display = "block";
+          }
+          slideSetting(++currentSlide);
         } else {
-          location.href = "./result.html?level=4";
+          if (total < -12) {
+            location.href = "./result.html?level=1";
+          } else if (total < 0) {
+            location.href = "./result.html?level=2";
+          } else if (total < 12) {
+            location.href = "./result.html?level=3";
+          } else {
+            location.href = "./result.html?level=4";
+          }
         }
       }
+    });
+  });
+
+  // 뒤로가기;
+  document.querySelector("#prevSlide").addEventListener("click", () => {
+    if (currentSlide === 3) {
+      const prevSlideBtn = document.getElementById("prevSlide");
+      prevSlideBtn.style.display = "none";
     }
+    const headerRef = document.getElementById("header");
+    headerRef.style.display = "block";
+    slideSetting(--currentSlide);
   });
 });
-
-// 뒤로가기;
-const prevSlide = () => {
-  const prevSlideBtn = document
-    .getElementById("prevSlide")
-    .addEventListener("click", () => {
-      const beforeSlide = document.getElementById(`slide_${currentSlide}`);
-      beforeSlide.classList.remove("activeSlide");
-
-      const beforeSlideRef = document.getElementById(
-        `slide_${currentSlide - 1}`
-      );
-      beforeSlideRef.classList.add("activeSlide");
-    });
-};
-prevSlide();
